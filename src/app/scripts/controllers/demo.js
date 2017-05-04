1e7b11a0cd510b27df5fdea0ff7366be391d318f
Raymond <ray@firestitch.com>
2017-05-03 20:49:14 -0400

    initial commit

---
+++ src/app/scripts/controllers/demo.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+angular.module('app')
+  .controller('DemoCtrl', function ($scope) {
+
+
+    $scope.text = '';
+
+    $scope.submit = function() {
+        alert('submit');
+    }
+
+    $scope.textelement = {};
+
+    setTimeout(function() {
+    	$scope.textelement.focus();
+    },2000)
+});
+
---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-element',[])
+	.directive('fsElement', function () {
+		return {
+			scope: {
+				instance: '=fsElement',
+			},
+		 	link: function ($scope, element) {
+		 		angular.extend($scope.instance,
+		 			{
+		     			scroll: {
+		     				bottom: function() {
+		     					element[0].scrollTop = element[0].scrollHeight;
+		     				},
+		     				top: function() {
+		     					element[0].scrollTop = 0;
+		     				}
+		     			},
+		     			focus: function() {
+		     				element[0].focus();
+		     			}
+		     		});
+			}
+		};
+	});
+})();
